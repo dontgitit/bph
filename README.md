ad31212489e82bdcc6b39f47f06944ba92af8eb8
bar <thisisntreal@fake.com>
2013-04-29 18:05:07 -0700

    charting earthquakes/day for last 7 days

--- README.md
+++ README.md
@@ -2,9 +2,12 @@ bph
 ===
 A project that allows user to view a graph of earthquakes per day for the past seven days, as well as a list of the ten most recent earthquakes.
 
-This project requires web2py to run. 
+This project requires web2py to run. Additionally, it requires the flot.js charting library.
 Simply download web2py and unzip. Use "python we2py.py" to run. In web2py/applications, create a symlink to the location of the git repo (so your tree should look like this)
        web2py/applications/bph -> symlink-to-repo-dir/controllers/top10.py
        web2py/applications/bph -> symlink-to-repo-dir/models/top10/index.html
 
-To access, go to http://127.0.0.1:8000/bph/top10
+web2py should have made a bunch of folder for you in the repo dir. Unzip the flot.js library into static/js, so your tree looks like this:
+       web2py/applications/bph -> symlink-to-repo-dir/static/js/flot/jquery.flot.min.js
+
+To access, go to http://127.0.0.1:8000/bph/top10 or http://127.0.0.1:8000/bph/recent7
--- controllers/recent7.py
+++ controllers/recent7.py
@@ -1,21 +1,26 @@
 import urllib2
 import json
 from datetime import datetime, timedelta
+import calendar
 
 def index():
-    url_file = urllib2.urlopen("http://www.seismi.org/api/eqs?limit=400")
+    url_file = urllib2.urlopen("http://www.seismi.org/api/eqs?limit=100")
     json_data = url_file.read()
     results = json.loads(json_data)
     eqs = results['earthquakes']
-    # create a list of earthquakes for each day
+    # create a list of earthquakes for each day (keys are milliseconds since midnight in UTC)
     days_to_eqs = dict()
-    dt = datetime.now()
+    dt = datetime.utcnow().replace(hour=0, minute=0, second=0)
     for i in range(0, 7):
-        dt_str = dt.strftime("%Y-%m-%d")
-        days_to_eqs[dt_str] = 0
+        #dt_str = dt.strftime("%Y-%m-%d")
+        #days_to_eqs[dt_str] = 0
+        dt_ms_utc = calendar.timegm(dt.timetuple()) * 1000
+        days_to_eqs[dt_ms_utc] = 0
         dt = dt - timedelta(days = 1)
     for eq in eqs:
-        eq_date, eq_time = eq["timedate"].split()
-        if eq_date in days_to_eqs:
-            days_to_eqs[eq_date] += 1
-    return dict(days_to_eqs = days_to_eqs)
+        eq_date_str, eq_time = eq["timedate"].split()
+        eq_dt = datetime.strptime(eq_date_str + " UTC", "%Y-%m-%d %Z")
+        eq_ms_utc = calendar.timegm(eq_dt.timetuple()) * 1000
+        if eq_ms_utc in days_to_eqs:
+            days_to_eqs[eq_ms_utc] += 1
+    return dict(days_to_eqs = sorted(days_to_eqs.iteritems()))
